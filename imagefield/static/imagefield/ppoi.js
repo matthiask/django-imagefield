0d1de0d192398ceec4062b185fedfad9bca333e5
Matthias Kestenholz <mk@feinheit.ch>
2018-03-22 21:58:56 +0100

    Factor the PPOI point style setting

--- imagefield/static/imagefield/ppoi.js
+++ imagefield/static/imagefield/ppoi.js
@@ -1,26 +1,30 @@
 ;(function() {
 
   function centerpointWidget() {
+    function movePoint(point, img, x, y) {
+      point.style.left = (img.clientWidth * x) + 'px';
+      point.style.top = (img.clientHeight * y) + 'px';
+
+    }
+
     Array.prototype.slice.call(document.querySelectorAll(
       '.imagefield[data-ppoi-id]'
     )).forEach(function(field) {
       var ppoiField = document.querySelector('input[name="' + field.dataset.ppoiId + '"]');
       if (!ppoiField) return;
 
-      var matches = ppoiField.value.match(/^([\.0-9]+)x([\.0-9]+)$/);
-      var x, y;
-      if (matches) {
-        x = parseFloat(matches[1]);
-        y = parseFloat(matches[2]);
-      } else {
-        x = 0.5;
-        y = 0.5;
-      }
-
       var point = document.createElement('div'),
         img = field.querySelector('img');
       point.className = 'imagefield-point';
       img.parentNode.appendChild(point);
+
+      var matches = ppoiField.value.match(/^([\.0-9]+)x([\.0-9]+)$/);
+      var x, y;
+      if (matches) {
+        movePoint(point, img, parseFloat(matches[1]), parseFloat(matches[2]));
+      } else {
+        movePoint(point, img, .5, .5);
+      }
 
       point.style.left = (img.clientWidth * x) + 'px';
       point.style.top = (img.clientHeight * y) + 'px';
@@ -38,8 +42,7 @@
         x = e.offsetX / img.clientWidth;
         y = e.offsetY / img.clientHeight;
         ppoiField.value = x.toFixed(2) + 'x' + y.toFixed(2);
-        point.style.left = (img.clientWidth * x) + 'px';
-        point.style.top = (img.clientHeight * y) + 'px';
+        movePoint(point, img, x, y);
       }
     });
 
