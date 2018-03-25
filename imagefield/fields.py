9eb68c1b9b4e528271970c3103e9609acde05387
Matthias Kestenholz <mk@feinheit.ch>
2018-03-25 23:01:33 +0200

    Possibly working automatic processing and deleting

--- imagefield/fields.py
+++ imagefield/fields.py
@@ -6,6 +6,7 @@ from types import SimpleNamespace
 from django.conf import settings
 from django.core.files.base import ContentFile
 from django.db import models
+from django.db.models import signals
 from django.db.models.fields import files
 from django.forms import ClearableFileInput
 from django.utils.functional import cached_property
@@ -52,6 +53,10 @@ class ImageFieldFile(files.ImageFieldFile):
 
         return '__processed__/%s/%s_%s%s' % (p1[:2], p1[2:], p2, ext)
 
+    def _processed_base(self):
+        p1 = urlhash(self.name)
+        return '__processed__/%s' % p1[:2], '%s_' % p1[2:]
+
     def process(self, item, force=False):
         processors = self.field.formats[item]
         target = self._processed_name(processors)
@@ -76,10 +81,7 @@ class ImageFieldFile(files.ImageFieldFile):
                 self.storage.delete(target)
                 self.storage.save(target, ContentFile(buf.getvalue()))
 
-    def save(self, name, content, save=True):
-        super().save(name, content, save=True)
-        for key in self.field.formats:
-            self.process(key)
+                print('Saved', target)
 
 
 class ImageField(models.ImageField):
@@ -106,17 +108,23 @@ class ImageField(models.ImageField):
             self._formats,
         )
 
-    """
     def contribute_to_class(self, cls, name, **kwargs):
         super().contribute_to_class(cls, name, **kwargs)
-        print(self, self.__dict__)
 
-        signals.post_init
-        signals.post_save
-        signals.post_delete
+        if not cls._meta.abstract:
+            # TODO Avoid calling process() too often?
+            # signals.post_init.connect(self.cache_values, sender=cls)
 
-        -> detect ppoi changes
-    """
+            # TODO Allow deactivating this by to move it out of the
+            # request-response cycle.
+            signals.post_save.connect(
+                self._generate_files,
+                sender=cls,
+            )
+            signals.post_delete.connect(
+                self._clear_generated_files,
+                sender=cls,
+            )
 
     def formfield(self, **kwargs):
         kwargs['widget'] = with_preview_and_ppoi(
@@ -127,9 +135,24 @@ class ImageField(models.ImageField):
 
     def save_form_data(self, instance, data):
         super().save_form_data(instance, data)
+
+        # Reset PPOI field if image field is cleared
         if data is not None and not data:
             if self.ppoi_field:
                 setattr(instance, self.ppoi_field, '0.5x0.5')
+
+    def _generate_files(self, instance, **kwargs):
+        f = getattr(instance, self.name)
+        for item in f.field.formats:
+            f.process(item)
+
+    @staticmethod
+    def _clear_generated_files(instance, **kwargs):
+        f = getattr(instance, self.name)
+        folder, startswith = f._processed_name()
+        for file in f.storage.listdir(folder):
+            if file.startswith(startswith):
+                f.storage.delete(file)
 
 
 class PPOIField(models.CharField):
