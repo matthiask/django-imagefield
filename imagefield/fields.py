matthiask/django-imagefield

commit 824f28a002f73fc0888fdc141c6761ec3cb41a9d

Bail out when image format starts with an underscore

imagefield/fields.py
@@ -126,9 +126,9 @@ class ImageFieldFile(files.ImageFieldFile):
         # The "field" attribute is not there after unpickling. We cannot
         # help in this case so let's just raise an AttributeError and leave the
         # rest to the FileDescriptor
-        if item == "field":
-            raise AttributeError
-        if item in self.field.formats:
+        if item == "field" or item.startswith("_"):
+            pass
+        elif item in self.field.formats:
             context = self._process_context(self.field.formats[item])
             if context.name:
                 url = self.storage.url(context.name)

commit 0e5c3a320b33de7ee5f886536988cc368322eeb4

Unconditionally raise exceptions if self.field does not exist
Fixes a regression in 824f28a002f73fc0888fdc141c6761ec3cb41a9d

imagefield/fields.py
@@ -126,9 +126,9 @@ class ImageFieldFile(files.ImageFieldFile):
         # The "field" attribute is not there after unpickling. We cannot
         # help in this case so let's just raise an AttributeError and leave the
         # rest to the FileDescriptor
-        if item == "field" or item.startswith("_"):
-            pass
-        elif item in self.field.formats:
+        if item == "field":
+            raise AttributeError
+        if not item.startswith("_") and item in self.field.formats:
             context = self._process_context(self.field.formats[item])
             if context.name:
                 url = self.storage.url(context.name)