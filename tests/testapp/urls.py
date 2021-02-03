99529c1e65e2c0d7de6ec8b27bbbc76ecae3a275
Matthias Kestenholz <mk@feinheit.ch>
2021-02-03 19:48:03 +0100

    Add Django 3.2a1, fix one problem with Django@master

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,14 +1,19 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+
+
+try:
+    from django.urls import re_path
+except ImportError:
+    from django.conf.urls import url as re_path
 
 
 # from testapp import views
 
 
-urlpatterns = [url(r"^admin/", admin.site.urls)] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+urlpatterns = [
+    re_path(r"^admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
